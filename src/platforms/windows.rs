8240265d07488bc8fa0be7b988924e34ce9cfe47
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-24 20:14:27 -0400

    Fix windows, not implemented yet

--- src/platforms/windows.rs
+++ src/platforms/windows.rs
@@ -1,42 +1,22 @@
-use crate::common::{App, PlatformContext, PlatformTrait};
+use crate::common::App;
 use std::path::PathBuf;
-use toml;
-use walkdir::WalskDir;
+// use walkdir::WalskDir;
+use anyhow::Result;
 
-#[cfg(target_os = "windows")]
 pub fn get_apps() -> Vec<App> {
     vec![]
 }
 
-#[cfg(target_os = "windows")]
-pub fn open_file_with(file_path: PathBuf, app_path: PathBuf) {}
+pub fn open_file_with(file_path: PathBuf, app: App) {}
 
+pub fn get_frontmost_application() -> Result<App> {
+    todo!()
+}
 
-#[cfg(test)]
-mod tests {
-    use std::path::PathBuf;
+pub fn get_all_apps() -> Result<Vec<App>> {
+    Ok(vec![])
+}
 
-    use super::{get_apps, open_file_with};
-
-    #[test]
-    fn test_get_apps() {
-        let apps = get_apps();
-        for app in apps {
-            println!("App: {:?}", app);
-        }
-    }
-
-    // #[test]
-    // fn ios_app() {
-    //     let path = PathBuf::from("/Applications/Surge.app");
-    //     let found = find_ios_app_icon(path);
-    //     println!("Found: {:?}", found);
-    // }
-
-    // #[test]
-    // fn open_file_with_vscode() {
-    //     let file_path = PathBuf::from("/Users/hacker/Desktop/new_IQA.py");
-    //     let app_path = PathBuf::from("/Applications/Visual Studio Code.app");
-    //     super::open_file_with(file_path, app_path);
-    // }
+pub fn get_running_apps() -> Vec<App> {
+    vec![]
 }
